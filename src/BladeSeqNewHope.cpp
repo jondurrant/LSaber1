e696455e4892ba463454f6f2b4069717074254a6
Jon Durrant <jon@jondurrant.com>
2021-12-08 14:51:22 +0000

    Restructured to allow base class with most of animation code

--- src/BladeSeqNewHope.cpp
+++ src/BladeSeqNewHope.cpp
@@ -17,47 +17,6 @@ BladeSeqNewHope::~BladeSeqNewHope() {
 	// TODO Auto-generated destructor stub
 }
 
-void BladeSeqNewHope::on(BladeColour *c, PicoLed::PicoLedController *strip, uint8_t length){
-	xBladeSeq = BladeSeqTurnOn;
-	xStartTime = to_ms_since_boot(get_absolute_time());
-}
-
-void BladeSeqNewHope::off(BladeColour *c, PicoLed::PicoLedController *strip, uint8_t length){
-	xBladeSeq = BladeSeqTurnOff;
-	xStartTime = to_ms_since_boot(get_absolute_time());
-}
-
-void BladeSeqNewHope::tick(BladeColour *c, PicoLed::PicoLedController *strip, uint8_t length){
-
-	if ((xBladeSeq == BladeSeqTurnOn) || (xBladeSeq == BladeSeqTurnOff)){
-		uint32_t sinceStart = to_ms_since_boot(get_absolute_time()) - xStartTime;
-
-		if (sinceStart > NEWHOPEMS){
-			if (xBladeSeq == BladeSeqTurnOff){
-				bladeOff(length, c, strip, length);
-				xBladeSeq = BladeSeqOff;
-			} else {
-				bladeOn(length, c, strip, length);
-				xBladeSeq - BladeSeqOn;
-			}
-			lastStep = 0;
-			strip->show();
-		} else {
-			uint32_t step =  sinceStart / (NEWHOPEMS / length);
-			if (step > lastStep){
-				if (xBladeSeq == BladeSeqTurnOff){
-					bladeOff(step, c, strip, length);
-				} else {
-					bladeOn(step, c, strip, length);
-				}
-				lastStep = step;
-				strip->show();
-			}
-		}
-	}
-}
-
-
 void BladeSeqNewHope::bladeOn(uint8_t count, BladeColour *c, PicoLed::PicoLedController *strip, uint8_t length){
 	uint8_t r,g,b;
 
@@ -71,3 +30,4 @@ void BladeSeqNewHope::bladeOff(uint8_t count, BladeColour *c, PicoLed::PicoLedCo
 	strip->clear();
 	bladeOn(length - count, c, strip, length);
 }
+
--- src/BladeSeqNewHope.h
+++ src/BladeSeqNewHope.h
@@ -8,29 +8,20 @@
 #ifndef BLADESEQNEWHOPE_H_
 #define BLADESEQNEWHOPE_H_
 
-#include "BladeSeqInterface.h"
+#include "BladeSeqAnim.h"
 
 #define NEWHOPEMS 500
 
-class BladeSeqNewHope : public BladeSeqInterface {
+class BladeSeqNewHope : public BladeSeqAnim {
 public:
 	BladeSeqNewHope();
 	virtual ~BladeSeqNewHope();
 
-	virtual void on(BladeColour *c, PicoLed::PicoLedController *strip, uint8_t length);
+protected:
 
-	virtual void off(BladeColour *c, PicoLed::PicoLedController *strip, uint8_t length);
+	virtual void bladeOn(uint8_t count, BladeColour *c, PicoLed::PicoLedController *strip, uint8_t length);
+	virtual void bladeOff(uint8_t count, BladeColour *c, PicoLed::PicoLedController *strip, uint8_t length);
 
-	virtual void tick(BladeColour *c, PicoLed::PicoLedController *strip, uint8_t length);
-
-private:
-
-	BladeSeqEnum xBladeSeq = BladeSeqOff;
-	uint32_t xStartTime = 0; //MS
-	uint32_t lastStep = 0;
-
-	void bladeOn(uint8_t count, BladeColour *c, PicoLed::PicoLedController *strip, uint8_t length);
-	void bladeOff(uint8_t count, BladeColour *c, PicoLed::PicoLedController *strip, uint8_t length);
 };
 
 #endif /* BLADESEQNEWHOPE_H_ */
